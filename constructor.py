8de40ef9538a848eafbebb637375cb975c60a0e2
nagasai8722 <167081917+nagasai8722@users.noreply.github.com>
2024-04-20 11:28:28 +0530

    Create constructor

---
+++ constructor.py
@@ -0,0 +1,9 @@
+class Box:
+    def __init__(self,nameofStudent,rollno):
+        self.name = nameofStudent
+        self.rollno = rollno
+
+obj1 = Box("govind",76)
+obj2=Box("raj",54)
+print(obj1.name)
+print(obj2.name)
